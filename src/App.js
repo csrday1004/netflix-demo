4cf97a8579c2bd8fac8d45761182891755098c80
JSR <csrday1004@gmail.com>
2024-04-07 01:10:18 +0900

    마지막과제-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import AppLayout from './layout/AppLayout';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage/Homepage';
+import Movies from './pages/Movies/Movies';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+
+//홈페이지 /
+//전체영화 페이지(서치) /movies 
+//디테일 페이지 /movies/:id
+//추천영화 /movies/:id/recommandation
+//리뷰 /movies/:id/reviews
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
+    <Routes>
+      <Route path='/' element={<AppLayout/>}>
+        <Route index element={<Homepage/>}/>
+        <Route path='movies'>
+          <Route index element={<Movies/>}/>
+          <Route path=':id' element={<MovieDetail/>}/>
+        </Route>
+      </Route>
+
+      <Route path='*' element={<NotFoundPage/>}/>
+    </Routes>
     </div>
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+//npm i react-dom react-router-dom
+import { BrowserRouter } from "react-router-dom";
+//npm i react-bootstrap bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//npm i axios
+//npm i @tanstack/react-query
+//npm i @tanstack/react-query-devtools
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
